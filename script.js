959ba130c90fd7e409e7b07c39566cd6651f3f8d
calledphilly <134594230+calledphilly@users.noreply.github.com>
2023-05-30 12:24:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ window.addEventListener("scroll", () => {
     let paragraphs = document.querySelectorAll(".scrolling-paragraph");
     let viewportHeight = window.innerHeight/3;
 
-    /* "querySelectorAll()" selectionne plusieurs choses en même temps, à linverse de "querySelector()"
+    /* "querySelectorAll()" selectionne plusieurs choses en même temps, à l'inverse de "querySelector()"
     qui n'en selectionne qu'un seul.
-    Il est donc necessaire avant d'utiliser un evenement de les décomposer en plusieurs
-    avec "forEach()". de donner un a chaque element decomposer,
-    puis ensuite d'ajouter l'evenement a ce nouveau nom de variable.
+    Il est donc nécéssaire avant d'utiliser un evenement de les décomposer en plusieurs
+    avec "forEach()". de donner un a chaque element décomposer,
+    puis ensuite d'ajouter l'evenement à ce nouveau nom de variable.
     */
 
     paragraphs.forEach((paragraph) => {
